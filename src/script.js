0d4f73a246862675f51103266a15b2f64bc2e2e3
cslylla <81328857+cslylla@users.noreply.github.com>
2021-08-15 21:47:52 +0200

    Adding JS tp play and pause video

---
+++ src/script.js
@@ -0,0 +1,18 @@
+let playPauseButton = document.getElementById("mediaControl");
+let video = document.getElementById("heroVideo");
+let buttonBg = document.getElementById("buttonSymbol")
+
+function playPause(){
+    if (video.paused === false) {
+    buttonBg.setAttribute("src", "/media/Pause.svg");
+    video.pause();
+  } else {
+    buttonBg.setAttribute("src", "/media/Play.svg");
+    video.play();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+
+
